30cfa7cc0ebc868070a739cd0381889056177bc9
vagnercruz <vagnerdacruz48@gmail.com>
2025-04-16 21:35:02 -0300

    adiconando a lógica ao modelo habit e completation

--- app/models/completion.rb
+++ app/models/completion.rb
@@ -1,3 +1,4 @@
 class Completion < ApplicationRecord
   belongs_to :habit
+  validates :completed_on, uniqueness: { scope: :habit_id }
 end
--- app/models/habit.rb
+++ app/models/habit.rb
@@ -1,2 +1,15 @@
 class Habit < ApplicationRecord
+  has_many :completions, dependent: :destroy
+
+  def completed_on?(date)
+    completions.exists?(completed_on: date)
+  end
+
+  def toggle_completion(date)
+    if completed_on?(date)
+      completions.find_by(completed_on: date)&.destroy
+    else
+      completions.create(completed_on: date)
+    end
+  end
 end
