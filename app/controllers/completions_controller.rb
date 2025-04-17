248c84995270eda24e1a1d1a3c7d418f1d1d37ab
vagnercruz <vagnerdacruz48@gmail.com>
2025-04-16 21:47:31 -0300

    Adicionando a regra aos controladores

--- app/controllers/completions_controller.rb
+++ app/controllers/completions_controller.rb
@@ -1,4 +1,8 @@
 class CompletionsController < ApplicationController
   def toggle
+    habit = Habit.find(params[:habit_id])
+    date = Date.parse(params[:date])
+    habit.toggle_completion(date)
+    redirect_to habit_path(habit)
   end
 end
--- app/controllers/habits_controller.rb
+++ app/controllers/habits_controller.rb
@@ -1,13 +1,29 @@
 class HabitsController < ApplicationController
   def index
+    @habits = Habit.all
   end
 
   def show
+    @habit = Habit.find(params[:id])
+    @dates = (0..6).map { |i| Date.today - i }
   end
 
   def new
+    @habit = Habit.new
   end
 
   def create
+    @habit = Habit.new(habit_params)
+    if @habit.save
+      redirect_to habits_path, notice: "Hábito criado!"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def habit_params
+    params.require(:habit).permit(:name, :user)
   end
 end
